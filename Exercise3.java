0decd3b489d02065c7a782435abcb1bca8cfa752
Bhagat Nath <128309383+bhagatsnath@users.noreply.github.com>
2025-11-11 10:49:18 -0500

    Rename class from Practice1 to Exercise3

--- Exercise3.java
+++ Exercise3.java
@@ -4,7 +4,7 @@
  * Java Unit 4 Practice
  */
 import java.util.*;
-public class Practice1{
+public class Exercise3{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         int i = 1;
